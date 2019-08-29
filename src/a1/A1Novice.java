3e90a447dbf88b10fdcc487df9aa351cabfc7e56
katjane <katjane@katjane-pc.wireless-1x.unc.edu>
2019-08-29 16:24:43 -0400

    Try again

--- src/a1/A1Novice.java
+++ src/a1/A1Novice.java
@@ -5,62 +5,44 @@ import java.util.Scanner;
 public class A1Novice {
 
 	public static void main(String[] args) {
-		
-//		Scanner scanny = new Scanner(System.in);
-//		
-//		int numCustomers = scanny.nextInt();
-//		for (int i = 0; i < numCustomers; i++) {
-//			String firstName = scanny.next();
-//			char firstChar = firstName.charAt(0);
-//			String lastName = scanny.next();
-//			int totalSpent = 0;
-//			int totalItems = scanny.nextInt();
-//			for (int j = 0; j < totalItems; j++) {
-//				int quantOfItem = scanny.nextInt();
-//				String itemName = scanny.next();
-//				double priceOfItem = scanny.nextDouble();
-//				totalSpent += quantOfItem * priceOfItem;
-//			}
-//			System.out.println(firstChar + ". " + lastName + ": " + totalSpent);
-		
+
 		Scanner scan = new Scanner(System.in);
-		
+
 		int numCustomers = scan.nextInt();
-		String[] stats = new String[numCustomers];
+		//		String[] stats = new String[numCustomers];
 		for (int i = 0; i < numCustomers; i++) {
-			// problem is somewhere after this line, within last for-loop?
 			String firstName = scan.next().charAt(0) + ". ";
 			String lastName = scan.next() + ": ";
 			int numItems = scan.nextInt();
 			double totalSpent = 0.0;
 			for (int j = 0; j < numItems; j++) {
 				int quantItem = scan.nextInt();
-				
+
 				//here the problem is!
 				String nameItem = scan.next();
 				// the problem is this line!!
 				// it only occurs with the very last grocery item, it's like 
 				// the scanner doesn't read the last line 
-				
-				System.out.println("No problems yet!");
+
+				//				System.out.println("No problems yet!");
 				double itemPrice = scan.nextDouble();
 				double spentOnItem = quantItem * itemPrice;
 				totalSpent = totalSpent + spentOnItem;
+				//			stats[i] = firstName + lastName + totalSpent;
+
+
 			}
-			stats[i] = firstName + lastName + totalSpent;
 			System.out.println(firstName + lastName + totalSpent);
-			
-		
+
+			//		for (int k = 0; k < numCustomers; k++) {
+			//			System.out.println(stats[k]);
+			//		}
+			//		
+			// Your code goes here.
 		}
-		
-		for (int i = 0; i < numCustomers; i++) {
-			System.out.println(stats[i]);
-		}
-		
-		// Your code goes here.
+
+		// You can define / use static helper methods here.
 	}
-	
-	// You can define / use static helper methods here.
 }
 
 //4
