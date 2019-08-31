52d0885c246e7fbdcebc2d2b4687b84cefbddaec
katjane <katjane@rachels-air-3.wireless-1x.unc.edu>
2019-08-31 09:05:40 -0400

    implemented first draft of Adept functions

--- src/a1/A1Jedi.java
+++ src/a1/A1Jedi.java
@@ -1,12 +1,12 @@
 package a1;
 
+import java.text.DecimalFormat;
 import java.util.Scanner;
 
 public class A1Jedi {
 
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
-		
 		// Your code goes here.
 	}
 	
